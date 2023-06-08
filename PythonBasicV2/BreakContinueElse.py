5f5acfb48a0397c0419d2ced5e6fadcfb847b6eb
Ahmadov Mahammad Azarovic <ahmadov_m@itstep.edu.az>
2023-06-08 19:36:50 +0400

    BreakContinueElse

---
+++ PythonBasicV2/BreakContinueElse.py
@@ -0,0 +1,33 @@
+# BREAK  - полностью прерывает цикл и выходит из не
+
+# while True:
+#     num_1 = int(input("NUM 1 : "))
+#     num_2 = int(input("NUM 2 : "))
+#     if num_1 == 0:
+#         break
+#     print(num_1+num_2)
+
+
+# CONTINUE - пропускает одну итерацию цикла
+
+# i = 0
+#
+# while i < 20:
+#     i+=1
+#     if i%2!=0:
+#      continue
+#     print(i)
+
+
+# ELSE - выполняется только в том случае , если цикл полностью и корректно сработал
+
+# i = 0
+#
+# while i < 20:
+#     i += 1
+#     if i==10:
+#         break
+#     print(i)
+#
+# else:
+#     print("Cikl polnostyu srabotal")
