2815c5693d452aeae9a9a77fdd9379112735a67f
Ahmadov Mahammad Azarovic <ahmadov_m@itstep.edu.az>
2023-08-03 20:20:06 +0400

    C++

--- C++ Basic/first_lesson/first_lesson/lesson_01_08_23.cpp
+++ C++ Basic/first_lesson/first_lesson/lesson_01_08_23.cpp
@@ -1,54 +1,54 @@
-#include<iostream>
-
-using namespace std;
-
-int main() 
-{
-
-	int num_1 = 10000000000000;
-
-	cout << num_1 << endl << endl;
-
-	float  num_2 = 12.5453453;
-
-	cout << num_2 << endl << endl;
-
-	double num_3 = 12.884555;
-	cout << "double : " << endl;
-	printf("%.9f", num_3);
-	cout << endl << endl;
-	cout << num_3 << endl << endl;
-
-	long long num_4 = 2147483649;
-	cout << "long long : " << endl;
-	cout << "sizeof - " << sizeof(num_4) << endl;
-	cout << num_4 << endl << endl;
-
-	unsigned int num_5 = 2147483649;
-	cout << "unsigned int : " << endl;
-	cout << "sizeof - " << sizeof(num_5) << endl;
-	cout << num_5 << endl << endl;
-
-	bool isNum = true;
-	cout << "bool : " << endl;
-	cout << "sizeof - " << sizeof(isNum) << endl;
-	cout << isNum << endl << endl;
-
-	char sym = 'a';
-	cout << "char : " << endl;
-	cout << "sizeof - " << sizeof(sym) << endl;
-	cout << sym << endl << endl;
-
-	short int num_7 = 3434;
-	cout << "short int : " << endl;
-	cout << "sizeof - " << sizeof(num_7) << endl;
-	cout << num_7 << endl << endl;
-
-	char  sym_2 = 'a';
-	cout << sym_2 << endl << endl;
-
-	char  sym_3 = 65;
-	char  sym_4 = 33;
-	cout << sym_3 << sym_4 << endl << endl;
-	cout << char(sym_3 + sym_4) << endl << endl;
-}
+//#include<iostream>
+//
+//using namespace std;
+//
+//int main() 
+//{
+//
+//	int num_1 = 10000000000000;
+//
+//	cout << num_1 << endl << endl;
+//
+//	float  num_2 = 12.5453453;
+//
+//	cout << num_2 << endl << endl;
+//
+//	double num_3 = 12.884555;
+//	cout << "double : " << endl;
+//	printf("%.9f", num_3);
+//	cout << endl << endl;
+//	cout << num_3 << endl << endl;
+//
+//	long long num_4 = 2147483649;
+//	cout << "long long : " << endl;
+//	cout << "sizeof - " << sizeof(num_4) << endl;
+//	cout << num_4 << endl << endl;
+//
+//	unsigned int num_5 = 2147483649;
+//	cout << "unsigned int : " << endl;
+//	cout << "sizeof - " << sizeof(num_5) << endl;
+//	cout << num_5 << endl << endl;
+//
+//	bool isNum = true;
+//	cout << "bool : " << endl;
+//	cout << "sizeof - " << sizeof(isNum) << endl;
+//	cout << isNum << endl << endl;
+//
+//	char sym = 'a';
+//	cout << "char : " << endl;
+//	cout << "sizeof - " << sizeof(sym) << endl;
+//	cout << sym << endl << endl;
+//
+//	short int num_7 = 3434;
+//	cout << "short int : " << endl;
+//	cout << "sizeof - " << sizeof(num_7) << endl;
+//	cout << num_7 << endl << endl;
+//
+//	char  sym_2 = 'a';
+//	cout << sym_2 << endl << endl;
+//
+//	char  sym_3 = 65;
+//	char  sym_4 = 33;
+//	cout << sym_3 << sym_4 << endl << endl;
+//	cout << char(sym_3 + sym_4) << endl << endl;
+//}
