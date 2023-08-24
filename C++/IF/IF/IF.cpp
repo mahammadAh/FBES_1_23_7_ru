8aeb3e741deed808f19be09fde0960fb226b383e
Ahmadov Mahammad Azarovic <ahmadov_m@itstep.edu.az>
2023-08-24 21:35:53 +0400

    if else

---
+++ C++/IF/IF/IF.cpp
@@ -0,0 +1,150 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = -1;
+
+    //if (a++)
+    //{
+    //    cout << "Ura !" << endl;
+    //}
+
+    //if (++a)
+    //{
+    //    cout << "Ura !" << endl;
+    //}
+     
+    a = 5;
+
+    //if (a=0)
+    //{
+    //    cout << "Ura !" << endl;
+    //}
+
+    //if (a = 2)
+    //{
+    //    cout << "Ura !" << endl;
+    //}
+
+    a = 0;
+    int b = 0;
+
+    //if (a == b)
+    //{
+    //    cout << "Ura !" << endl;
+    //}
+
+    //a = 13;
+
+    //if (3 < a && a < 20)
+    //{
+    //    cout << "Ura !" << endl;
+    //}
+
+    int month = 20;
+
+    //if (month == 1 || month == 2 || month == 12)
+    //{
+    //    cout << "Winter" << endl;
+    //}
+
+    //if (month >= 3 && month <= 5)
+    //{
+    //    cout << "Spring" << endl;
+    //}
+
+    //if (month >= 6 && month <= 8)
+   
+    //
+
+    //if (month >= 9 && month <= 11)
+    //{
+    //    cout << "Autumn" << endl;
+    //}
+
+   // month = 11;
+
+    //month == 8 ? cout << "August" : cout << "!August";
+
+    //month >= 6 && month <= 8 ? month == 8 ? cout << "August" : cout << "June or July" : cout << "!Summer";
+
+    //if (month == 8)
+    //{
+    //    cout << "August";
+    //}
+    //else
+    //{
+    //    cout << "!August";
+    //}
+    // условие ? условие выполняется : условие не выполняется
+
+
+    //month = 3;
+
+    //if (month == 1)
+    //{
+    //    cout << "January";
+    //}
+    //else { if (month == 2)
+    //    {
+    //        cout << "February";
+    //    }
+    //    else
+    //    {
+    //        if (month == 3)
+    //        {
+    //            cout << "March";
+    //        }
+    //        else
+    //        {
+    //            if (month == 4)
+    //            {
+    //                cout << "April";
+    //            }
+    //            else
+    //            {
+    //                if (month == 5)
+    //                {
+    //                    cout << "May";
+    //                }
+    //            }
+    //        }
+    //    }
+    //}
+
+    //if (month == 1)
+    //{
+    //    cout << "January";
+    //}
+    //else if (month == 2)
+    //{
+    //    cout << "February";
+    //}
+    //else if (month == 3)
+    //{
+    //    cout << "March";
+    //}
+    //else if (month == 4)
+    //{
+    //    cout << "April";
+    //}
+    //else if (month == 5)
+    //{
+    //    cout << "May";
+    //}
+    //else
+    //{
+    //    cout << "Error" << endl;
+    //}
+
+
+
+
+
+
+
+}
+
+
