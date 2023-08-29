147b0193fced47d5621373946043288ebe8c997f
Ahmadov Mahammad Azarovic <ahmadov_m@itstep.edu.az>
2023-08-29 21:48:44 +0400

    while/do while

---
+++ C++/While/While/While.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // while
+    // do while
+    // for
+
+
+    //float money = 50;
+    //float book = 6;
+    //int bookCounter = 0;
+
+    //while (money >= 6)
+    //{
+    //    money -= book;  // money = money - book
+    //    bookCounter++;
+    //    cout << "This is your - " << bookCounter << " Book" << endl;
+    //    cout << "You have " << money << " AZN" << endl;
+    //}
+
+    //int studentCount = 0;
+
+
+    //while (true)
+    //{
+    //    studentCount+=100000;
+    //    cout << studentCount<<endl;
+    //}
+
+    //int year = 0;
+    //while (year <= 2023)
+    //{
+    //    if (year % 4 == 0)
+    //    {
+    //        cout << year<<endl;
+    //    }
+    //    year+=4;
+    //    
+    //}
+
+ /*   int age = 23;
+
+    while (age < 25)
+    {
+        cout <<"Your age - "<<age<<" ABB Tam Genc" << endl;
+        age++;
+    }*/
+
+    // while , for - циклы с предусловием
+    // do while - циклы с постусловием
+
+    //int age = 23;
+    //do
+    //{
+    //    cout << "Your age - " << age << " ABB Tam Genc" << endl;
+    //    age++;
+    //} while (age < 25);
+
+    //int dateOfBirth;
+
+    //while (dateOfBirth < 2023)
+    //{
+    //    cin >> dateOfBirth;
+    //    cout << "age : " << 2023 - dateOfBirth;
+    //}
+
+
+    //do
+    //{
+    //    cin >> dateOfBirth;
+    //    cout << "age : " << 2023 - dateOfBirth<<endl;
+    //} while (dateOfBirth < 2023);
+
+
+    //int childAge;
+
+    //do
+    //{
+    //    cout << "Child Age : ";
+    //    cin >> childAge;
+    //    if (childAge >= 5 && childAge <= 7)
+    //    {
+    //        cout << "Success";
+    //    }
+    //} while (childAge >= 5 && childAge <= 7);
+
+
+    //int i = 0;
+    //while (i < 10)
+    //{
+    //    i++;
+    //    if (i == 7) {
+    //        break;
+    //    }
+    //    cout << i << endl;
+
+    //}
+
+
+    //int i = 5;
+    //while (i < 10)
+    //{
+    //    i++;
+    //    cout << i << endl;
+    //}
+//0 Ч черный
+//1 Ч синий
+//2 Ч зеленый
+//3 Ч голубой
+//4 Ч красный
+//5 Ч лиловый
+//6 Ч желтый
+//7 Ч белый
+//8 Ч серый
+//9 Ч свело - синий
+//A Ч светло - зеленый
+//B Ч светло - голубой
+//— Ч светло - красный
+//E Ч светло - желтый
+//F Ч €рко - белый
+
+while (true)
+{
+    char sym;
+    cin >> sym;
+    system("color F0");
+}
+
+
+}
+
+
