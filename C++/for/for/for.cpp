408b6f0116141c17372070b6c556daaaa4c77b76
Ahmadov Mahammad Azarovic <ahmadov_m@itstep.edu.az>
2023-08-31 20:54:51 +0400

    for

---
+++ C++/for/for/for.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	
+	//for (int i = 0; i < 10; i++)
+	//{
+	//	cout << "i = " << i<<endl;
+	//}
+
+	//int i;
+	//for (i = 0; i < 10; i++)
+	//{
+	//	cout << "i = " << i<<endl;
+	//}
+
+	//int i;
+	//i = 0;
+	//for (;i < 10; i++)
+	//{
+	//	cout << "i = " << i << endl;
+	//}
+
+
+	//for (int i = 0;; i++)
+	//{
+	//	cout << "i = " << i<<endl;
+	//}
+
+
+	//for (int i = 0;;)
+	//{
+	//	cout << "i = " << i<<endl;
+	//}
+
+
+	//for (;;)
+	//{
+	//	cout << "i = "<<endl;
+	//	break;
+	//}
+
+	//for (int i = 0; i < 5 ; i++)
+	//{
+	//	cout << "i : " << i << endl;
+	//	for (int j = 0; j < 10; j++)
+	//	{
+	//		cout << "j : " << j << endl;
+	//	}
+	//	
+	//}
+
+	//int magicNum = 7;
+	//int userNum;
+	//cout << "MAGIC GAME" << endl;
+
+	//for (int i = 1; i < 6; i++)
+	//{
+	//	cout << "chance - " << i << endl;
+	//	cout << "Your Number : ";
+	//	cin >> userNum;
+	//	if (userNum == magicNum)
+	//	{
+	//		cout << "You WIN !" << endl;
+	//		break;
+	//	}
+	//	else
+	//	{
+	//		if (i == 4) {
+	//			cout << "You lose..." << endl;
+	//		}
+	//		else
+	//		{
+	//			cout << "try again" << endl;
+	//		}
+	//		
+	//	}
+	//}
+
+	//int num1, num2, num3;
+	//int num1=2,num2=3,num3=4;
+
+
+	//for (int i = 5; i > -5; i--)
+	//{
+	//	if (i == 0) i--;
+	//	cout <<"20 / "<<i<<" = "<<20 / i << endl;
+	//}
+
+	for (int i = 0; i < 1000; i += 2)
+	{
+		if (i % 6 == 0) {
+			cout << i << endl;
+		}
+	}
+
+}
