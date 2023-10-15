0c48d026f9521da2088b480b1c0d42f7a734f88b
Ahmadov Mahammad Azarovic <ahmadov_m@itstep.edu.az>
2023-10-15 21:57:06 +0400

    2x Dynamic Array

---
+++ C++/2xDynamicArrays/2xDynamicArrays/2xDynamicArrays.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+void fillArray(int arr[][4], int size1, int size2) {
+	for (int i = 0; i < size1; i++) {
+		for (int j = 0; j < size2; j++) {
+			arr[i][j] = 50 - rand() % 100;
+		}
+	}
+
+}
+
+
+void printArray(int arr[][4], int size1, int size2) {
+	for (int i = 0; i < size1; i++) {
+		for (int j = 0; j < size2; j++) {
+			cout << arr[i][j] << "\t";
+		}
+		cout << endl;
+	}
+
+}
+
+void fillDynamicArray(int**& arr,int arrSize[], int size) {
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < arrSize[i]; j++) {
+			arr[i][j] =  rand() % 100;
+		}
+	}
+}
+
+void printDynamicArray(int**& arr, int arrSize[],int size) {
+	for (int i = 0; i < size; i++) {
+		cout << "size --> " << arrSize[i] << " : ";
+		for (int j = 0; j < arrSize[i]; j++) {
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
+
+int main()
+{
+	srand(time(NULL));
+	//
+	//int const size1 = 5;
+	//int const size2 = 4;
+
+	//int arr[size1][size2];
+
+	//fillArray(arr, size1, size2);
+	//printArray(arr, size1, size2);
+
+	int size = 15+rand()%25;
+	int* arrSize = new int[size]; // arr1 - pointer
+
+	int** arr = new int* [size]; // arr2 - pointer to pointer
+
+
+	for (int i = 0; i < size; i++) {
+		int newSize = 1 + rand() % 25;
+		arr[i] = new int[newSize];
+		arrSize[i] = newSize;
+	}
+
+	fillDynamicArray(arr, arrSize,size);
+	printDynamicArray(arr, arrSize, size);
+
+}
+
