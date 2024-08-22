061d2aa39b3aa65472f83b49b7c5afce7ce78677
Ahmadov Mahammad Azarovic <ahmadov_m@itstep.edu.az>
2024-08-22 20:17:20 +0400

    'SP'

---
+++ SP/FirstLesson/FirstLesson/Program.cs
@@ -0,0 +1,48 @@
+﻿
+
+using System.Diagnostics;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //Process.Start("notepad","text.txt");
+
+        var processes = Process.GetProcesses();
+
+        foreach  (var process in processes) 
+        {
+            try
+            {
+                //Console.WriteLine($"{process.Id} {process.ProcessName} {process.PriorityClass}");
+
+                //Console.WriteLine($"{process.ProcessName} {process.MainModule}");
+
+                //Console.WriteLine($"{process.ProcessName} :");
+
+                //foreach(var module in process.Modules)
+                //{
+                //    Console.WriteLine($"\t {module}");
+                //}
+
+
+                //Console.WriteLine($"{process.ProcessName} {process.TotalProcessorTime}:");
+
+
+                //if(process.ProcessName == "Notepad")
+                //{
+                //    process.Kill();
+                //}
+
+                Console.WriteLine($"{process.ProcessName} {process.TotalProcessorTime}:");
+            }
+            catch (Exception ex)
+            {
+
+            }
+            
+        }
+
+
+    }
+}
